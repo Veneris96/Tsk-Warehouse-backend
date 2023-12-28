53900ae5b7e5ff4bc47a552ba413c04b37d95761
Veneris96 <119521599+Veneris96@users.noreply.github.com>
2023-12-28 19:23:04 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const server = express()
 server.use(express.json())
 server.use(cors())
 
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGODB_URL);
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
 import usersRoute from "./Routes/users.js";
 import productsRoute from "./Routes/products.js"
 import loginRoute from "./Routes/login.js";
@@ -19,14 +29,16 @@ server.use("/", usersRoute)
 server.use("/", productsRoute)
 server.use("/", loginRoute)
 
-
-mongoose.connect(process.env.MONGODB_URL)
-
-
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "Connessione al database non riuscita."))
 db.once("open", () => {
     console.log("Connessione al database stabilita.")
 })
 
-server.listen(PORT, () => console.log(`Server avviato sulla porta ${PORT}.`))
+connectDB().then(() => {
+    server.listen(PORT, () => {
+        console.log(`Server avviato sulla porta ${PORT}.`);
+    })
+})
+
+// server.listen(PORT, () => console.log())
